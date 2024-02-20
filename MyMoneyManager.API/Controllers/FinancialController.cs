ea90226357b86b99f765825675f9e73953333839
kapilaku <37741394+kapilaku@users.noreply.github.com>
2024-02-20 10:33:55 +0800

    Complete the basic functionality

--- MyMoneyManager.API/Controllers/FinancialController.cs
+++ MyMoneyManager.API/Controllers/FinancialController.cs
@@ -5,6 +5,7 @@ using MyMoneyManager.API.Models;
 using MyMoneyManager.API.Services;
 using MyMoneyManager.Shared.ViewModels;
 using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -40,6 +41,107 @@ public class FinancialController : ControllerBase
         return GetFinancialStatus(accountId, DateTime.UtcNow.Date.AddDays(1), "year");
     }
 
+    [HttpGet("dailyzeros/{accountId}")]
+    public ICollection<FinancialStatusViewModel> GetDayilyZeros(int accountId)
+    {
+        return GetFinancialStatusWithZeros(accountId, DateTime.UtcNow.Date.AddDays(1), "day");
+    }
+    [HttpGet("monthlyzeros/{accountId}")]
+    public ICollection<FinancialStatusViewModel> GetMonthlyZeros(int accountId)
+    {
+        return GetFinancialStatusWithZeros(accountId, DateTime.UtcNow.Date.AddDays(1), "month");
+
+    }
+    [HttpGet("yearlyzeros/{accountId}")]
+    public ICollection<FinancialStatusViewModel> GetYearlyZeros(int accountId)
+    {
+        return GetFinancialStatusWithZeros(accountId, DateTime.UtcNow.Date.AddDays(1), "year");
+    }
+
+    private DateTime GetStartDate(string userId)
+    {
+        return _context.Split.Where(s => s.AppUserId == userId).Include(s => s.Transaction).First().Transaction.Occured;
+    }
+    private ICollection<FinancialStatusViewModel> GetFinancialStatusWithZeros(int accountId, DateTime currentDate, string interval)
+    {
+        var userId = _userService.GetUserId(HttpContext);
+        DateTime startTime = GetStartDate(userId);
+
+        var originalResult = _context.Transaction
+            .Where(t => t.AppUserId == userId &&
+                        t.Splits.Any(s => s.AccountId == accountId) &&
+                        t.Occured >= startTime &&
+                        t.Occured <= currentDate)
+            .Include(t => t.Splits)
+            .OrderBy(t => t.Occured)
+            .AsEnumerable()
+            .GroupBy(t => GetGroupingKey(t.Occured, interval))
+            .Select(group => new FinancialStatusViewModel
+            {
+                TimePeriod = group.Key,
+                FundChange = group.Sum(t => t.Splits.Where(s => s.AccountId == accountId)
+                                                    .Sum(s => s.Balance)),
+                CurrentBalance = CalculateRunningTotal(_context.Transaction.Where(t => t.AppUserId == userId &&
+                                                                               t.Splits.Any(s => s.AccountId == accountId) &&
+                                                                               t.Occured >= startTime &&
+                                                                               t.Occured <= currentDate)
+                                                  .Include(t => t.Splits)
+                                                  .AsEnumerable()
+                                                  .GroupBy(t => GetGroupingKey(t.Occured, interval)), accountId, group.Key)
+            })
+            .ToList();
+
+        var existingTimePeriods = originalResult.Select(item => item.TimePeriod).ToList();
+
+        var allTimePeriods = GetPossibleTimePeriods(startTime, currentDate.AddDays(1), interval).ToList();
+
+        var missingTimePeriods = allTimePeriods.Except(existingTimePeriods);
+
+        foreach (var missingTimePeriod in missingTimePeriods)
+        {
+            originalResult.Add(new FinancialStatusViewModel
+            {
+                TimePeriod = missingTimePeriod,
+                FundChange = 0,
+                CurrentBalance = 0
+            });
+        }
+        var finalResult = originalResult.OrderBy(item => item.TimePeriod).ToList();
+        return finalResult;
+    }
+
+    private List<DateTime> GetPossibleTimePeriods(DateTime startDate, DateTime endDate, string interval)
+    {
+        List<DateTime> possibleTimePeriods = new List<DateTime>();
+
+        DateTime currentDate = startDate;
+        while (currentDate <= endDate)
+        {
+            possibleTimePeriods.Add(GetGroupingKey(currentDate, interval));
+            currentDate = GetTimePeriodSpan(interval, currentDate);
+        }
+
+        return possibleTimePeriods;
+    }
+    private DateTime GetTimePeriodSpan(string interval, DateTime currentDateTime)
+    {
+        if (interval == "day")
+        {
+            return currentDateTime.AddDays(1);
+        }
+        if (interval == "month")
+        {
+            return currentDateTime.AddMonths(1);
+        }
+        if (interval == "year")
+        {
+            return currentDateTime.AddYears(1);
+        }
+
+        return currentDateTime.AddDays(1);
+    }
+
+
     private ICollection<FinancialStatusViewModel> GetFinancialStatus(int accountId, DateTime currentDate, string interval)
     {
         var userId = _userService.GetUserId(HttpContext);
@@ -59,20 +161,28 @@ public class FinancialController : ControllerBase
                 TimePeriod = group.Key,
                 FundChange = group.Sum(t => t.Splits.Where(s => s.AccountId == accountId)
                                                     .Sum(s => s.Balance)),
-                CurrentBalance = CalculateRunningTotal(group, accountId, group.Key)
+                CurrentBalance = CalculateRunningTotal(_context.Transaction.Where(t => t.AppUserId == userId &&
+                                                                               t.Splits.Any(s => s.AccountId == accountId) &&
+                                                                               t.Occured >= startTime &&
+                                                                               t.Occured <= currentDate)
+                                                  .Include(t => t.Splits)
+                                                  .AsEnumerable()
+                                                  .GroupBy(t => GetGroupingKey(t.Occured, interval)), accountId, group.Key)
             })
             .ToList();
         return result;
     }
 
-    private decimal CalculateRunningTotal(IGrouping<DateTime, Transaction> group, int accountId, DateTime currentTime)
+    private decimal CalculateRunningTotal(IEnumerable<IGrouping<DateTime, Transaction>> groups, int accountId, DateTime currentTime)
     {
         decimal runningTotal = 0;
-        foreach (var transaction in group.OrderBy(t => t.Occured))
+        //runningTotal += group.Sum(g => g.Sum(t =>  t.Splits.Where(s => s.AccountId == accountId).Sum(s => s.Balance)));
+        foreach (var group in groups.OrderBy(g => g.Key))
         {
-            if (transaction.Occured <= currentTime)
+
+            if (group.Key <= currentTime)
             {
-                runningTotal += transaction.Splits.Where(s => s.AccountId == accountId).Sum(s => s.Balance);
+                runningTotal += group.Sum(t => t.Splits.Where(s => s.AccountId == accountId).Sum(s => s.Balance));
             }
         }
         return runningTotal;
@@ -86,7 +196,7 @@ public class FinancialController : ControllerBase
             .OrderBy(t => t.Occured)
             .FirstOrDefault();
 
-        return earliestTransaction?.Occured.Date ?? DateTime.Now.Date;
+        return earliestTransaction?.Occured.Date ?? DateTime.UtcNow.Date;
     }
 
     private DateTime GetGroupingKey(DateTime transactionDate, string interval)
